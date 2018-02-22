8ba9e3e2833814d9e8689931dc2f70ec8741fc6a
DESKTOP-U4S59NP\Vitalii <vitaliy.melnychuk@xloo.com>
2018-02-22 17:06:04 +0200

    task 6 quiz 1

---
+++ sql-ex-site/T6_Q1.sql
@@ -0,0 +1,18 @@
+DROP FUNCTION IF EXISTS `FC_GET_TRIANGLE_AREA`;
+
+DELIMITER //
+CREATE FUNCTION `FC_GET_TRIANGLE_AREA` (
+  side1 FLOAT,
+  side2 FLOAT,
+  side3 FLOAT
+) RETURNS FLOAT
+  BEGIN
+    DECLARE half_perimiter FLOAT;
+    DECLARE area FLOAT;
+    SET @half_perimiter = (side1 * side2 * side3) / 2;
+    SET @area = SQRT( @half_perimiter * (@half_perimiter - side1) * (@half_perimiter - side2) * (@half_perimiter - side3));
+    RETURN @area;
+  END //
+DELIMITER ;
+
+SELECT FC_GET_TRIANGLE_AREA(23.43, 34.23, 43.34) AS triangle_area;
