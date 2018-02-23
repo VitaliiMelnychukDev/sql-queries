6ce838e6fa6856d24eeecb2b3942e3922b62c1c7
DESKTOP-U4S59NP\Vitalii <vitaliy.melnychuk@xloo.com>
2018-02-23 10:37:05 +0200

    Fix sql query

--- sql-ex-site/T7_Q1.sql
+++ sql-ex-site/T7_Q1.sql
@@ -9,10 +9,10 @@ CREATE FUNCTION `FC_GET_TRIANGLE_AREA` (
   BEGIN
     DECLARE half_perimiter FLOAT;
     DECLARE area FLOAT;
-    SET @half_perimiter = (side1 * side2 * side3) / 2;
-    SET @area = SQRT( @half_perimiter * (@half_perimiter - side1) * (@half_perimiter - side2) * (@half_perimiter - side3));
-    RETURN @area;
+    SET half_perimiter = (side1 * side2 * side3) / 2;
+    SET area = SQRT( half_perimiter * (half_perimiter - side1) * (half_perimiter - side2) * (half_perimiter - side3));
+    RETURN area;
   END //
 DELIMITER ;
 
-SELECT FC_GET_TRIANGLE_AREA(23.43, 34.23, 43.34) AS triangle_area;
+SELECT FC_GET_TRIANGLE_AREA(23.93, 34.23, 43.34) AS triangle_area;
